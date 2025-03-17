dc948e7b597d8433ca5dda00e647193bc92c7017
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2025-03-17 09:18:46 +0700

    add : register dbcontext viewmodel + page

--- HRMapp/MauiProgram.cs
+++ HRMapp/MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using HRMapp.Data.Database;
+using HRMapp.Pages;
+using HRMapp.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace HRMapp;
 
@@ -15,8 +19,21 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
+        // Register AppDbContext with MySQL
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseMySql("server=localhost;user=root;password=;database=hrmseiden",
+                             new MySqlServerVersion(new Version(10, 4, 28))));
+
+        // Register ViewModels
+        builder.Services.AddSingleton<EmployeeListViewModel>();
+        builder.Services.AddTransient<EmployeeDetailViewModel>();
+
+        // Register Pages
+        builder.Services.AddSingleton<ManageEmployee>();
+        builder.Services.AddTransient<EmployeeDetailPage>();
+
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
---
+++ HRMapp/Pages/EmployeeDetailPage.cs
@@ -0,0 +1,15 @@
+namespace HRMapp.Pages;
+
+public class EmployeeDetailPage : ContentPage
+{
+	public EmployeeDetailPage()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
