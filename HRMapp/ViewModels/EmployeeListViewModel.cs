ccdc622893730f8f0b2cb13475c6599b4395361f
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2025-03-19 15:39:33 +0700

    change : datagrid view for ManageEmployee page
    using akgul.maui.datagrid package

--- HRMapp/ViewModels/EmployeeListViewModel.cs
+++ HRMapp/ViewModels/EmployeeListViewModel.cs
@@ -22,8 +22,6 @@ namespace HRMapp.ViewModels
         [ObservableProperty]
         private ObservableCollection<Employee> employees = new();
 
-        [ObservableProperty]
-        private ObservableCollection<Employee> filteredEmployees = new();
 
         [ObservableProperty]
         private string searchText = string.Empty;
@@ -31,86 +29,31 @@ namespace HRMapp.ViewModels
         [ObservableProperty]
         private Employee selectedEmployee;
 
-        public ICommand LoadMoreCommand { get; }
-        public ICommand SearchCommand { get; }
+        
         public ICommand EmployeeSelectedCommand { get; }
 
         public EmployeeListViewModel(IDbContextFactory<AppDbContext> dbContextFactory)
         {
             _dbContextFactory = dbContextFactory;
-            LoadMoreCommand = new AsyncRelayCommand(LoadEmployeeAsync);
-            SearchCommand = new RelayCommand(FilterEmployees);
+            LoadEmployeeAsync();
             EmployeeSelectedCommand = new AsyncRelayCommand(NavigateToEmployeeDetail);
         }
-
-        private int _pageSize = 20;
-        private int _currentPage = 1;
-        private bool _isLoading = false;
 
         [RelayCommand]
         private async Task LoadEmployeeAsync()
         {
-            if (_isLoading) return;
-            _isLoading = true;
+            using var dbContext = _dbContextFactory.CreateDbContext();
 
-            try
-            {
-                using var dbContext = _dbContextFactory.CreateDbContext();
+            var employeeList = await dbContext.Employees
+                .AsNoTracking()
+                .Include(e => e.Department)
+                .Include(e => e.Job)
+                .Include(e => e.Factory)
+                .OrderBy(e => e.employee_id)
+                .ToListAsync();
 
-                var employeeList = await dbContext.Employees
-                    .AsNoTracking()
-                    .Include(e => e.Department)
-                    .Include(e => e.Job)
-                    .Include(e => e.Factory)
-                    .OrderBy(e => e.employee_id)
-                    .Skip((_currentPage - 1) * _pageSize)
-                    .Take(_pageSize)
-                    .ToListAsync();
-
-                MainThread.BeginInvokeOnMainThread(() =>
-                {
-                    if (_currentPage == 1)
-                        Employees.Clear();
-                    foreach (var emp in employeeList)
-                        Employees.Add(emp);
-                });
-
-                FilterEmployees();
-
-                _currentPage++;
-
-                //employees = new ObservableCollection<Employee>(employeeList);
-                //OnPropertyChanged(nameof(Employees));
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error loading employees: {ex.Message}");
-            }
-            finally
-            {
-                _isLoading = false;
-            }
-        }
-
-        private void FilterEmployees()
-        {
-            if (string.IsNullOrWhiteSpace(SearchText))
-            {
-                // If search text is empty, show full list
-                FilteredEmployees = new ObservableCollection<Employee>(Employees);
-            }
-            else
-            {
-                var lowerText = SearchText.ToLower();
-                FilteredEmployees = new ObservableCollection<Employee>(
-                    Employees.Where(e =>
-                        e.name.ToLower().Contains(lowerText) ||
-                        e.nip.ToLower().Contains(lowerText) ||
-                        (e.Department != null && e.Department.name.ToLower().Contains(lowerText)) ||
-                        (e.Job != null && e.Job.job_name.ToLower().Contains(lowerText))
-                    )
-                );
-            }
+            employees = new ObservableCollection<Employee>(employeeList);
+            OnPropertyChanged(nameof(Employees));
 
         }
 
