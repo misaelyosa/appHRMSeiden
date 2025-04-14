f68c4f2241d2d125bd6251ca6efd3eaf31226624
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2025-04-14 10:57:40 +0700

    add : query insert employee, todo validasi input if new city, province, ed

--- HRMapp/ViewModels/EmployeeFormViewModel/CreateEmployeeViewModel.cs
+++ HRMapp/ViewModels/EmployeeFormViewModel/CreateEmployeeViewModel.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,7 +40,7 @@ namespace HRMapp.ViewModels.EmployeeFormViewModel
         [ObservableProperty]
         private City selectedCity;        
         [ObservableProperty]
-        private City selectedProvince;
+        private Province selectedProvince;
         [ObservableProperty]
         private Education selectedEducation;
         [ObservableProperty]
@@ -53,22 +54,12 @@ namespace HRMapp.ViewModels.EmployeeFormViewModel
         [ObservableProperty]
         private string selectedEmployeeStatus;
 
-        [ObservableProperty]
-        private string newDepartment;
-        [ObservableProperty]
-        private string newJob;
-        [ObservableProperty]
-        private string newReligion;
+
         [ObservableProperty]
         private string newCityName;
         [ObservableProperty]
         private string newProvinceName;
-        [ObservableProperty]
-        private string newFactoryName;
-        [ObservableProperty]
-        private string newFactoryAddress;
-        [ObservableProperty]
-        private int newFactoryCapacity;
+
         [ObservableProperty]
         private string newEducationType;
         [ObservableProperty]
@@ -111,6 +102,14 @@ namespace HRMapp.ViewModels.EmployeeFormViewModel
             Religions = new(context.Religions.ToList());
         }
 
+        partial void OnSelectedCityChanged(City city)
+        {
+            if (SelectedCity != null)
+            {
+                SelectedProvince = Provinces.FirstOrDefault(p => p.province_id == SelectedCity.province_id);
+            }
+        }
+
         [RelayCommand]
         private async Task SubmitAsync()
         {
@@ -118,12 +117,37 @@ namespace HRMapp.ViewModels.EmployeeFormViewModel
             {
                 name = NewEmployee.name,
                 nip = NewEmployee.nip,
-                Department = SelectedDepartment
+                nik = NewEmployee.nik,
+                phone_number = NewEmployee.phone_number,
+                email = NewEmployee.email,
+                birthplace = NewEmployee.birthplace,
+                birthdate = SelectedBirthdate,
+                address = NewEmployee.address,
+                marital_status = NewEmployee.marital_status,
+                graduation_date = SelectedGraduationDate,
+                gender = SelectedGender,
+
+                department_id = SelectedDepartment.department_id,
+                job_id = SelectedJob.job_id,
+                religion_id = SelectedReligion.religion_id,
+                city_id = SelectedCity.city_id,
+                factory_id = SelectedFactory.factory_id,
+                education_id = SelectedEducation.education_id,
+                employee_status = SelectedEmployeeStatus
             };
+
+            //TODO Bikin validasi input new city, province --> insert new object if new
+
+            //Debug.WriteLine($"City: {insertEmployee.City?.city_name}");
+            //Debug.WriteLine($"Province: {insertEmployee.City.Provinces.province_name}");
+            //Debug.WriteLine($"Education Type: {insertEmployee.Education?.education_type}");
+            //Debug.WriteLine($"Major: {insertEmployee.Education?.major}");
 
             await _employeeService.CreateEmployeeAsync(insertEmployee);
             await Shell.Current.GoToAsync("..");
+
         }
+        
 
         [RelayCommand]
         private async Task Back()
