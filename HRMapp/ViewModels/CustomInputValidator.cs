2d3103859a04da9c08cdc1ee91498163e59ebf11
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2025-05-05 11:10:06 +0700

    create : custom input validator

---
+++ HRMapp/ViewModels/CustomInputValidator.cs
@@ -0,0 +1,25 @@
+﻿using InputKit.Shared.Validations;
+
+namespace HRMapp.ViewModels
+{
+    public class NullableNumericValidator : IValidation
+    {
+        public string? Message { get; set; } = "Input must be numeric";
+
+        public bool Validate(object value)
+        {
+            if (value == null)
+                return true;
+
+            if (value is string str)
+            {
+                if (string.IsNullOrWhiteSpace(str))
+                    return true; // empty input is allowed
+
+                return double.TryParse(str, out _);
+            }
+
+            return false;
+        }
+    }
+}
