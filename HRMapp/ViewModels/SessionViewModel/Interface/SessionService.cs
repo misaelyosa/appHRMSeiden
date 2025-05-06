f724ee5727c963b573d6d9f89544eb6b20e3373f
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2025-05-06 09:37:09 +0700

    create : init session interface

---
+++ HRMapp/ViewModels/SessionViewModel/Interface/SessionService.cs
@@ -0,0 +1,23 @@
+﻿using HRMapp.Data.Database;
+using HRMapp.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMapp.ViewModels.SessionViewModel.Interface
+{
+    public class SessionService : ISessionService
+    {
+        private readonly IDbContextFactory<AppDbContext> _contextFactory;
+
+        public SessionService(IDbContextFactory<AppDbContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        
+    }
+}
